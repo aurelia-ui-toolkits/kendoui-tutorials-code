e203c4b54f72957c22cf6cab858ebdcee3ac2871
Nikolaj Ivancic <nik@congral.com>
2016-11-14 22:36:39 -0500

    add chart and grid

--- skeleton-esnext/src/app.js
+++ skeleton-esnext/src/app.js
@@ -6,7 +6,9 @@ export class App {
       { route: 'users',         name: 'users',        moduleId: 'users',                                nav: true, title: 'Github Users' },
       { route: 'child-router',  name: 'child-router', moduleId: 'child-router',                         nav: true, title: 'Child Router' },
       { route: 'autocomplete',  name: 'autocomplete', moduleId: 'kendoui/autocomplete/autocomplete',    nav: true, title: 'KendoUI autocomplete' },
-      { route: 'button',        name: 'button',       moduleId: 'kendoui/button/button',                nav: true, title: 'KendoUI button' }
+      { route: 'button',        name: 'button',       moduleId: 'kendoui/button/button',                nav: true, title: 'KendoUI button' },
+      { route: 'chart',         name: 'chart',        moduleId: 'kendoui/chart/chart',                  nav: true, title: 'KendoUI chart' },
+      { route: 'grid',          name: 'grid',         moduleId: 'kendoui/grid/grid',                    nav: true, title: 'KendoUI grid' }
     ]);
 
     this.router = router;
